f0b2c30c2ead2d6d081d0d0f1b58584ea05fb378
Kenefi <cszakavec5@gmail.com>
2025-02-07 12:19:30 -0500

    removed unused shader code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,6 @@
 
 #include "shader.h"
 
-// Vertex Shader Source Code
-const char *vertexShaderSource = R"(
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-    void main() {
-        gl_Position = vec4(aPos, 1.0);
-    }
-)";
-
-// Fragment Shader Source Code
-const char *fragmentShaderSource = R"(
-    #version 330 core
-    out vec4 FragColor;
-    void main() {
-        FragColor = vec4(1.0, 0.5, 0.2, 1.0);
-    }
-)";
-
 void key_callback(GLFWwindow *window, int key, int scancode, int action,
                   int mods) {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
