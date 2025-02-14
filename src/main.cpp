c270fdfb8cc967bc09c31343634b1a6f5ff3d77f
Kenefi <cszakavec5@gmail.com>
2025-02-14 18:53:21 -0500

    rendering a square

--- src/main.cpp
+++ src/main.cpp
@@ -38,13 +38,17 @@ int main() {
     std::cerr << "Failed to initialize GLAD\n";
     return -1;
   }
-
-  // Triangle Vertices
+ 
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f, // Bottom left
-      0.5f,  -0.5f, 0.0f, // Bottom right
-      0.0f,  0.5f,  0.0f  // Top
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left
   };
+  unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+  };  
 
   //
   //  SHADER
@@ -52,10 +56,11 @@ int main() {
   Shader paddleShader("../resources/shaders/paddle.vert",
                       "../resources/shaders/paddle.frag");
 
-  // Create VAO and VBO
-  unsigned int VAO, VBO;
+  // Create VAO, VBO, and EBO
+  unsigned int VAO, VBO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   // Bind VAO
   glBindVertexArray(VAO);
@@ -63,6 +68,10 @@ int main() {
   // Bind and set VBO data
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Bind and set EBO data
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   // Set vertex attribute pointers
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
@@ -77,9 +86,9 @@ int main() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     // Use shader program and draw triangle
-    glUseProgram(paddleShader.ID);
+    paddleShader.use();
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     // Swap buffers and poll events
@@ -90,6 +99,7 @@ int main() {
   // Cleanup
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
   glDeleteProgram(paddleShader.ID);
   glfwDestroyWindow(_window);
   glfwTerminate();
